2af3cc1d64294080e8f815acb9a46b6b3b526100
trkinvincible <trkinvincible85@gmail.com>
2014-08-14 19:32:51 +0800

    Tried Class

---
+++ FirstCSharpProgram/BusCaseOne.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstCSharpProgram
+{
+    public class Test
+    {
+        public Test()
+        {
+        }
+        public Int32 m_billAmount;
+        public Int32 Multiply()
+        {
+            return (5*m_billAmount);
+        }
+    }
+    class BusCaseOne
+    {
+        static void Main(string[] args)
+        {
+            Test testObj = new Test {m_billAmount = 10};
+            Console.WriteLine("Bill Amount is : {0}",testObj.Multiply());
+            Console.ReadKey(true);
+        }
+    }
+}
