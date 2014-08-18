a95d14116ce10a21f8213711db9754594197ae77
trkinvincible <trkinvincible85@gmail.com>
2014-08-18 14:25:17 +0800

    completed for Int

--- FirstCSharpProgram/SquareRoot.cs
+++ FirstCSharpProgram/SquareRoot.cs
@@ -9,8 +9,9 @@ namespace FirstCSharpProgram
     {
         public class FindRandom
         {
-            public static Int32 m_startRange;
-            public static Int32 m_endRange;
+            public static Random m_range = new Random();
+            public static Int32 m_startRange = 1;
+            public static Int32 m_endRange = 1;
 
             public FindRandom()
             {
@@ -19,26 +20,39 @@ namespace FirstCSharpProgram
             }
             public static Int32 GetRandomNumber(Int32 start,Int32 End)
             {
-                Random Range = new Random();
-                Int32 randG = Range.Next((int)start, (int)End);
-                return randG;            
+                Int32 randG = m_range.Next((int)start, (int)End);
+                return randG;               
             }
         }
         static void Main()
         {
+            Int32 randG;
+            Int32 tempRand;
+            UInt16 iterations = 0;
+            Console.WriteLine("Enter the Number to find Square root");
             Int32 inputN = Convert.ToInt32(Console.ReadLine());
-            Random Range = new Random();
-            Int32 randG = Range.Next((int)FindRandom.m_startRange, (int)inputN);
-            Int32 tempRand = randG * randG;
-            if (tempRand == inputN)
+            FindRandom.m_endRange = inputN;
+            while (true)
             {
-                Console.WriteLine("Square root of {0} is {1}", inputN, randG);
+                iterations++;
+                randG = FindRandom.GetRandomNumber((int)FindRandom.m_startRange, (int)FindRandom.m_endRange);
+                tempRand = randG * randG;
+                if (tempRand == inputN)
+                {
+                    Console.WriteLine("Square root of {0} is {1}", inputN, randG);
+                    Console.WriteLine("Number of Iterations {0}", iterations);
+                    break;
+                }
+                else if (tempRand > inputN)
+                {
+                    FindRandom.m_endRange = randG;
+                }
+                else
+                {
+                    FindRandom.m_startRange = randG;
+                }
             }
-            else if (tempRand > inputN)
-            {
-                FindRandom.m_endRange = randG;
-                randG = Range.Next((int)FindRandom.m_startRange, (int)FindRandom.m_endRange);
-            }
+            Console.ReadKey();
         }
     }
 }
