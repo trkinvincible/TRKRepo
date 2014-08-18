86cb7c3d50bcd26ad2a479cf718b18093e5db91a
trkinvincible <trkinvincible85@gmail.com>
2014-08-18 23:40:37 +0800

    initial

---
+++ FirstCSharpProgram/BussinessCaseTwo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstCSharpProgram
+{
+    class BussinessCaseTwo
+    {
+        public static class Address
+        {
+            public static Int32  m_blockNo;
+            public static Int32  m_code;
+            public static String m_address;
+        }
+        static void Main()
+        {
+            Console.WriteLine("Address database");
+            Console.WriteLine("Enter Block : ");
+            UInt32 block = UInt32.Parse(Console.Read().ToString());
+            Console.WriteLine("Enter Code : ");
+            String code = Console.Read().ToString();
+            String[] codearray = code.Split(' ');
+            if (code[1] != null)
+            {
+                Address.m_code = Int32.Parse(code[1].ToString());
+            }
+
+            Address.m_code += String." ";
+
+        }
+    }
+}
